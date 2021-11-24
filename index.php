19f16270316751851cecb11738fb3ea2ca43d2c3
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-11-24 15:30:39 -0800

    fixed merge conflict with Marcaus, added the viewer's view in category page

--- index.php
+++ index.php
@@ -4,35 +4,37 @@
 ?>
 
 <!-- Check if user is viewer or signed in, show corresponding categories-->
-
+<!-- Populate the "content" div with categories, topics, and posts based on if user is signed in or not -->
 <div id="content"> 
     <?php
-            if(isset($_SESSION['signed_in'])) //If signed in, show all categories
+            if(isset($_SESSION['signed_in']))
             {
-
+                //grab all categories from 'posts' where viewerviewable == true
                 $catQ = "SELECT ALL category FROM posts;";
 
                 $cats = $db->query($catQ);
 
 
                 if($cats == null){
-                    echo 'Something went wrong while accessing the database. Please try again later.';
-                } else {
-                    // echo '<br> made it into else <br>';
+                    echo 'Something went wrong while signing in. Please try again.';
+                }
+
+                else{
 
                     while($row = $cats->fetch(PDO::FETCH_ASSOC)){
                         $result[] = $row;
                     }
                     
-                    // Array of all column names (Super crucial for search bar as well!)
+                    // Array of all column names (Crucial for search bar!)
                     $columnArr = array_column($result, 'category');
-                        
+
                     for($i = 0; $i < count($columnArr); $i++){
-                        echo "<a href=\"topics.php\">".$columnArr[$i]."</a><br>";
+                        echo '<div class="category" name="'.$columnArr[$i].'"> <a class="categoryName" href="topics.php">'.$columnArr[$i].'</a></div>';
                     }
                 }
 
-            } else{ //If not signed in, still show all categories but when reaching viewerviewable==0(false), print out signin prompt
+            } else {
+                //If not signed in, still show all categories but when reaching viewerviewable==0(false), print out signin prompt
                 $catQ = "SELECT ALL category FROM posts;";
                 $cats = $db->query($catQ);
 
@@ -60,12 +62,12 @@
 
                     for($i = 0; $i < count($columnArr); $i++){
                         if($viewableArr[$i]==0) { //If viewerviewable==0(false), we tell viewer they need to signin
-                            echo "<p>".$columnArr[$i]." is only viewable when signed in.</p><br>";
+                            echo "<p> Topic: ".$columnArr[$i]." - must have an account and logged in to view!</p><br>";
                         } else { //otherwise, just show the value from 'category' with a link to topics.
                             echo "<a href=\"topics.php\">".$columnArr[$i]."</a><br>";
                         }
                     }
                 }
-            } 
-    ?>
+            }
+    ?>  
 </div>
