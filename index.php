ea824bb1f566e62186801166562e5f689df42f3d
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-11-26 15:13:43 -0800

    fixed duplicate form issue

--- index.php
+++ index.php
@@ -137,22 +137,7 @@
                         }
             }
     ?>
-            
+
 </div>
 </body>
 </html>
-
-<!-- Check to see if person is super user, if so, show "add category button" -->
-<?php
-    if(isset($_SESSION['signed_in']))
-    {
-        $vv = $_SESSION['permission'];
-        echo "Your permission status is: ".$vv."<br>";
-        
-        if($vv === "superuser") {
-            include 'addcatbtn.php';
-        }
-    } else {
-        echo "Your permission status is: viewer <br>";
-    }
-?>
