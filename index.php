372b880f63fb617cc190442e2a9b43aab566ac28
Haylee Rawdin <username@wwu.edu>
2021-11-26 15:09:32 -0800

    fixing error when there are no categories

--- index.php
+++ index.php
@@ -141,18 +141,3 @@
 </div>
 </body>
 </html>
-
-<!-- Check to see if person is super user, if so, show "add category button" -->
-<?php
-    if(isset($_SESSION['signed_in']))
-    {
-        $vv = $_SESSION['permission'];
-        echo "Your permission status is: ".$vv."<br>";
-        
-        if($vv === "superuser") {
-            include 'addcatbtn.php';
-        }
-    } else {
-        echo "Your permission status is: viewer <br>";
-    }
-?>
