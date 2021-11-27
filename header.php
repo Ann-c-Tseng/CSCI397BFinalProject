ec81cfddad4ed8eef6320a845cb555899dd7ecaa
Haylee Rawdin <username@wwu.edu>
2021-11-26 19:59:50 -0800

    working search bar

--- header.php
+++ header.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8" />
     <title>CSCI397B Final Project</title>
     <link rel="stylesheet" href="./styles.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <h1> CSCI397B Final Project | Argon </h1>
@@ -26,7 +27,13 @@ session_start();
                         else{
                             echo '<a href="signin.php">Sign in</a> or <a href="signup.php">Create an account</a>';
                         }
+                    echo '
+                        <button type="button" style="float:right" id="search" onclick="showfilter()";><i class="fa fa-search"></i></button>
+                        <button type="button" style="float:right; display:none" id="cancel" onclick="hidefilter()";><i class="fa fa-close"></i></button>
+                    ';
+                
                 ?>
+                
             </div>
         </div>
         <br><br>
